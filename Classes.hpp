9d0ed5a2da4df497b6cbea893d088e797f5c7abb
zheny-creator <rimkamix0@gmail.com>
2025-09-29 23:40:55 +0300

    Add comments to Book class, add_book function, and main.cpp explaining functionality

--- Classes.hpp
+++ Classes.hpp
@@ -1,19 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include "json.hpp"
-#pragma once
+#include <iostream>      // Для ввода/вывода (cout, cin)
+#include <vector>        // Для работы с динамическими массивами (в данном коде пока не используется)
+#include <string>        // Для работы со строками (в данном коде пока не используется)
+#include <unordered_map> // Для работы с ассоциативными массивами
+#include <fstream>       // Для работы с файлами
+#include "json.hpp"      // Для работы с JSON
+#pragma once             // Для того, чтобы компилятор не включал этот файл дважды
 
 using namespace std;
-
+using json = nlohmann::json;
 class Book
 {
-private:
-    string name;
-    string author;
-    string genre;
-    int year;
+protected:         // Поля доступны внутри класса и в наследниках
+    string name;   // Имя книги
+    string author; // Автор книги
+    string genre;  // Жанр книги
+    int year;      // Год издания книги
 
 public:
+    // Конструктор
     Book(const string &name, const string &author, const string &genre, int year) : name(name), author(author), genre(genre), year(year) {}
+
+    // Добавление книги в словарь
+    void add_book(const string &name, const string &author, const string &genre, int year)
+    {
+        unordered_map<string, Book> books;
+        books[name] = Book(name, author, genre, year);
+    }
 };
--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,11 @@
-#include "Classes.hpp"
+#include "Classes.hpp" // Здесь лежат классы
 
 int main()
 {
-    int choice;
-    string name, name_list, author, genre;
-    int year;
-    while (true)
+    int choice;                            // Переменная для выбора действия
+    string name, name_list, author, genre; // Переменные для хранения данных
+    int year;                              // Переменная для хранения года издания
+    while (true)                           // Бесконечный цикл
     {
         cout << "\n=== Меню ===\n";
         cout << "1: Создать список книг\n";
@@ -14,9 +14,9 @@ int main()
         cout << "4: Выйти\n";
         cout << "5: О программе\n";
         cout << "Введите номер действия: ";
-        cin >> choice;
+        cin >> choice; // Ввод выбора действия
 
-        if (choice == 1)
+        if (choice == 1) // Если выбрано создание списка книг
         {
             cout << "Введите название списка: ";
             getline(cin, name_list);
@@ -35,22 +35,22 @@ int main()
             cout << "В разработке!" << endl;
             continue;
         }
-        else if (choice == 2)
+        else if (choice == 2) // Если выбрано добавление книги в существующий список
         {
             cout << "В разработке!" << endl;
             continue;
         }
-        else if (choice == 3)
+        else if (choice == 3) // Если выбрано изменение книги в списке
         {
             cout << "В разработке!" << endl;
             continue;
         }
-        else if (choice == 4)
+        else if (choice == 4) // Если выбран выход
         {
             cout << "Выход из программы..." << endl;
             break;
         }
-        else if (choice == 5)
+        else if (choice == 5) // Если выбрана программа
         {
             cout << "Программа для работы с книгами." << endl;
             cout << "Автор: Женя Бородин" << endl;
